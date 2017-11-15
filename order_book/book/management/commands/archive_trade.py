37011f262ef9e14934b9b97fd19bc6ac8f4ec212
bear987978897 <bear987978897@gmail.com>
2017-11-16 01:50:00 +0800

    modify archive_trade.py

--- order_book/book/management/commands/archive_trade.py
+++ order_book/book/management/commands/archive_trade.py
@@ -6,31 +6,27 @@ time_interval = 15
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        last_timstamp = 0
+        last_timestamp = 0
         token_list = Token.objects.all()
         trade_buffer = []
         for i in token_list:
             trade_buffer.append([])
-        if not HistoryTrade.objects.exists():
-            first_trade = Trade.objects.all()[0]
-            last_timestamp = int(first_trade.timestamp/time_interval) * time_interval
-        else:
-            last_timestamp = HistoryTrade.objects.all()[-1].timestamp + time_interval
+        if HistoryTrade.objects.exists():
+            last_timestamp = list(HistoryTrade.objects.all().order_by('timestamp'))[-1].timestamp + time_interval
 
         while True:
-            next_timestamp = last_timestamp + time_interval
-            if (time.time() > (last_timestamp + int(time_interval / 3))):
+            if (time.time() > (last_timestamp + time_interval + 5)):
                 for ti in range(0, len(token_list)):
-                    trade_buffer[ti] = Trade.objects.filter(timestamp__gte=last_timestamp, timestamp__lt=next_timestamp, token_id=token_list[ti].id)
+                    trade_buffer[ti] = Trade.objects.filter(timestamp__gte=last_timestamp, token_id=token_list[ti].id).order_by('timestamp')
                     if not trade_buffer[ti]:
                         continue
-                    else:
-                        history_trade = reduce_to_history(trade_buffer[ti], last_timestamp)
-                        history_trade.save(force_insert=True)
+                    last_timestamp = int(trade_buffer[ti][0].timestamp / time_interval) *time_interval
+                    trade_buffer[ti] = trade_buffer[ti].filter(timestamp__lt=last_timestamp+time_interval)
+                    history_trade = reduce_to_history(list(trade_buffer[ti]), last_timestamp)
+                    history_trade.save(force_insert=True)
+                    last_timestamp = last_timestamp + time_interval
             else:
-                time.sleep(int(time_interval/6))
-                continue
-            last_timestamp = next_timestamp
+                time.sleep(5)
 
 
 
